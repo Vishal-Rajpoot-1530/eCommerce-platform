6e23526d1f9aadce55d332dd472ef9a0818b7134
VISHAL RAJPOOT <vishalrajpoot1530@gmail.com>
2024-12-13 12:34:35 +0530

    creat an eCommerce platform

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+ 
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+       Hello! This is an eCommerce platform .
+       <h1>Welcome to eCommerce</h1>
+    </>
+  )
+}
+
+export default App
